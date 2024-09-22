c96ca36ed9fa88e0c376e88939912a6576cefc81
PavithraPrasanth97 <pavithra19041997@gmail.com>
2024-09-22 18:33:28 +0530

    xoxo.js updated

--- script/xoxo.js
+++ script/xoxo.js
@@ -6,7 +6,7 @@ let board = ['', '', '', '', '', '', '', '', ''];
 let currentPlayer = 'X';
 let gameActive = true;
 
-const winningConditions=[
+const winningConditions = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -17,72 +17,72 @@ const winningConditions=[
     [2, 4, 6],
 ];
 
-cells.forEach((cell,index) =>{
-    cell.addEventListener('click',() => handleCellClick(index));
+cells.forEach((cell, index) => {
+    cell.addEventListener('click', () => handleCellClick(index));
 });
-function handleCellClick(index)
-{
-    if(board[index] !== '' || !gameActive) return;
 
-        board[index] = currentPlayer;
-        cellUpdate();
-        
-        currentPlayer = currentPlayer === 'X'?'O':'X';
-        if(currentPlayer === 'X')
-        {
+function handleCellClick(index) {
+    if (board[index] !== '' || !gameActive) return;
+
+    board[index] = currentPlayer;
+    cellUpdate();
+    checkResult();
+
+    if (gameActive) {
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        topMessageElement.innerText = `Player ${currentPlayer}'s turn`;
+    }
+}
+
+function cellUpdate() {
+    board.forEach((value, index) => {
+        cells[index].innerText = value;
+
+        // Add class based on the current player
+        if (value === 'X') {
             cells[index].classList.add('xfilled');
-        checkResult();
-        }
-        if(currentPlayer === 'O')
-        {
+            cells[index].classList.remove('ofilled');
+        } else if (value === 'O') {
             cells[index].classList.add('ofilled');
-        checkResult();
+            cells[index].classList.remove('xfilled');
         }
-
-        topMessageElement.innerText = `Player ${currentPlayer}'s turn` 
-
-}
-function cellUpdate()
-{
-    board.forEach((value,index)=>{
-    cells[index].innerText = value;
     });
-    
 }
-function checkResult()
-{
+
+function checkResult() {
     let roundWon = false;
-    for(let condition of winningConditions)
-    {
-        const [a, b, c]=condition;
-        if(board[a] === '' || board[b] === '' || board[c] === '') continue;
-        if(board[a] === board[b] && board[a] === board[c]) 
-        {
-            roundWon=true;
+    for (let condition of winningConditions) {
+        const [a, b, c] = condition;
+        if (board[a] === '' || board[b] === '' || board[c] === '') continue;
+        if (board[a] === board[b] && board[a] === board[c]) {
+            roundWon = true;
             break;
         }
     }
 
+    if (roundWon) {
+        messageElement.innerText = `Player ${currentPlayer} Wins!`;
+        gameActive = false;
+        return;
+    }
 
-if(roundWon)
-{
-    messageElement.innerText = `Player ${currentPlayer} Wins!`;
-    gameActive = false;
-    return;
-}
-if(!board.includes(''))
-    {
-        messageElement.innerText ='Draw';
-        gameActive=false;
+    if (!board.includes('')) {
+        messageElement.innerText = 'Draw';
+        gameActive = false;
     }
 }
-function resetGame()
-{
-    board =['','','','','','','','',''];
-    currentPlayer = 'X'
-    gameActive =true;
-    messageElement.innerText='';
+
+function resetGame() {
+    board = ['', '', '', '', '', '', '', '', ''];
+    currentPlayer = 'X';
+    gameActive = true;
+    messageElement.innerText = '';
+    topMessageElement.innerText = `Player ${currentPlayer}'s turn`;
     cellUpdate();
+
+    // Reset cell classes and content
+    cells.forEach(cell => {
+        cell.innerText = '';
+        cell.classList.remove('ofilled', 'xfilled');
+    });
 }
-cellUpdate();
-// handleCellClick();
