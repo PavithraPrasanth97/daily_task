58e3e69e514628c9628e11c0515745ce45a8d539
PavithraPrasanth97 <pavithra19041997@gmail.com>
2024-09-27 16:20:56 +0530

    js info task footer modified

--- script/js info task.js
+++ script/js info task.js
@@ -12,11 +12,21 @@ const Address = {
     addr: ["Salem", "Tamilnadu", "India"]
 };
 
+const Business_Hour= {
+    title: "Bussiness Hour",
+    content : [
+        "Monday-Friday : 9:00 - 18:00",
+        "Saturday : 9:00 - 16:00",
+        "Sunday : Closed"
+    ]
+};
+
 function displayInfo() {
     const footer = document.getElementById('footer');
 
+    // Create a div for courses
     const coursesDiv = document.createElement('div');
-    coursesDiv.classList.add('section', 'courses');
+    coursesDiv.classList.add('Coursesstyle');
 
     const coursesTitle = document.createElement('h1');
     coursesTitle.textContent = Courses.title;
@@ -28,15 +38,14 @@ function displayInfo() {
         const itemTitle = document.createElement('h4');
         itemTitle.textContent = course;
         listItem.appendChild(itemTitle);
-        listItem.style.listStyleType = 'none';
         coursesList.appendChild(listItem);
     });
     coursesDiv.appendChild(coursesList);
     footer.appendChild(coursesDiv);
 
-    
+    // Create a div for address
     const addressDiv = document.createElement('div');
-    addressDiv.classList.add('section', 'address');
+    addressDiv.classList.add( 'Addressstyle');
 
     const addressTitle = document.createElement('h1');
     addressTitle.textContent = Address.title;
@@ -48,11 +57,30 @@ function displayInfo() {
         const itemTitle = document.createElement('h4');
         itemTitle.textContent = addre;
         listItem.appendChild(itemTitle);
-        listItem.style.listStyleType = 'none';
         addressList.appendChild(listItem);
     });
     addressDiv.appendChild(addressList);
     footer.appendChild(addressDiv);
+
+
+      //create a div for business_hour
+    const BusinessDiv = document.createElement('div');
+    BusinessDiv.classList.add( 'Businessstyle');
+
+    const BusinessTitle = document.createElement('h1');
+    BusinessTitle.textContent = Business_Hour.title;
+    BusinessDiv.appendChild(BusinessTitle);
+
+    const BusinessList = document.createElement('ul');
+    Business_Hour.content.forEach(cont => {
+        const listItem = document.createElement('li');
+        const itemTitle = document.createElement('h4');
+        itemTitle.textContent = cont;
+        listItem.appendChild(itemTitle);
+        BusinessList.appendChild(listItem);
+    });
+    BusinessDiv.appendChild(BusinessList);
+    footer.appendChild(BusinessDiv);
 }
 
 window.addEventListener('load', displayInfo);
